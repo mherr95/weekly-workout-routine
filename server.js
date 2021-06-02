043dedcfbea5c8db8c3bdd98c1bb34da308416b8
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2021-06-01 22:09:48 -0400

    updating heroku

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const methodOverride = require('method-override');
 
 //App config
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/' + 'weeklyRoutine'
+
 
 //Middleware
 app.use(express.urlencoded({extended:true}));
@@ -12,7 +14,7 @@ app.use(methodOverride('_method'));
 app.use(express.static('public'));
 
 //External Middleware
-mongoose.connect('mongodb://localhost:27017/weeklyRoutine', {useNewUrlParser: true});
+mongoose.connect(MONGODB_URI , {useNewUrlParser: true});
 mongoose.connection.once('open', () => {
     console.log('connected to mongo');
 });
