6599d7d630871978743b489fe5f32425d4c3c561
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2021-06-02 12:23:34 -0400

    updating heroku

--- server.js
+++ server.js
@@ -4,23 +4,35 @@ const methodOverride = require('method-override');
 
 //App config
 const app = express();
+const db = mongoose.connection;
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/' + 'weeklyRoutine'
 
+//Connect to Mongo
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/' + 'weeklyRoutine'
 
 //Middleware
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
+app.use(express.json());
 
 //External Middleware
 mongoose.connect(MONGODB_URI , {useNewUrlParser: true});
-mongoose.connection.once('open', () => {
+db.once('open', () => {
     console.log('connected to mongo');
 });
 
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+
+//Controllers
 const daysController = require('./Controllers/dayOfWeek.js');
 app.use('/', daysController);
+
+
 
 app.listen(PORT, function() {
     console.log('Connected to Server');
