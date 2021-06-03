0746fb5007579674bb5a856a90b3549c3d74f16f
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2021-06-02 23:29:22 -0400

    adding css

--- models/day.js
+++ models/day.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema;
 const daySchema = new Schema ({
     day: {type: String, require: true},
     bodyPart: {type: String, require: true},
-    exercise: {type: String}
+    exercise: {type: String},
+    sets: {type: Number},
+    reps: {type: Number},
 },{timestamps: true});
 
 const Day = mongoose.model('day', daySchema);
